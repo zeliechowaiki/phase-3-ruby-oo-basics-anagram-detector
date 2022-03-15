a86356ffd4c615dc91f48ec1da5410f116033e24
Zelie Chowaiki <zeliechowaiki@gmail.com>
2022-03-15 17:17:55 -0400

    commit tree fba9facca56f265152a3b57963c8cfaa96e796e5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_list)
+    @matching_words = []
+    words_list.each do |w|
+      if w.chars.sort == @word.chars.sort
+        @matching_words << w
+      end
+    end
+    @matching_words
+  end
+end
+
+listen = Anagram.new("listen")
+puts listen.match(%w[enlists google inlets banana])
